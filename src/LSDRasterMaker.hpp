1dbc84bce3d0b5bcea64d6962a75a1e90d23227e
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2017-09-19 10:52:38 +0100

    First commit. Source code, boost minimal and readme

---
+++ src/LSDRasterMaker.hpp
@@ -0,0 +1,152 @@
+///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+///
+/// LSDRasterMaker.cpp
+/// header for the RasterMaker object
+/// The raster maker is a series of simple functions to make some rasters
+/// with different properties. 
+/// The initial use is mainly to make rasters for use in the raster model
+/// for uplift and K
+///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+///
+/// This object is written by
+/// @author Simon M. Mudd, University of Edinburgh
+///
+///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+///
+/// Version 0.0.1    01/09/2017
+///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include "TNT/tnt.h"
+#include "LSDRaster.hpp"
+using namespace std;
+using namespace TNT;
+
+#ifndef LSDRasterMaker_H
+#define LSDRasterMaker_H
+
+///@brief Create model objects to use LSDRaster methods on synthetic landscapes.
+class LSDRasterMaker: public LSDRaster
+{
+  public:
+
+    /// @brief Constructor. Create an LSDRasterMaker from an LSDRaster.
+    /// @return LSDRasterMaker
+    LSDRasterMaker()
+    {
+      create();
+    }
+
+
+    /// @brief Constructor. Create an LSDRasterMaker from an LSDRaster.
+    /// @return LSDRasterMaker
+    /// @param An_LSDRaster LSDRaster object.
+    LSDRasterMaker(LSDRaster& An_LSDRaster)
+    {
+      create(An_LSDRaster);
+    }
+
+    /// @brief Constructor. Create an LSDRasterMaker from a file.
+    /// Uses a filename and file extension
+    /// @return LSDRasterMaker
+    /// @param filename A String, the file to be loaded.
+    /// @param extension A String, the file extension to be loaded.
+    LSDRasterMaker(string filename, string extension)
+    {
+      create(filename, extension);
+    }
+
+    /// @brief Constructor. Create a blank raster nodel
+    /// @return LSDRasterModel
+    /// @param NCols Height of raster
+    /// @param NRows Width of raster
+    LSDRasterMaker(int NRows, int NCols)
+    {
+      create(NRows,NCols);
+    }
+
+    /// @brief This just returns the raster model object data as a raster
+    /// @return A raster with the data from the LSDRasterModel
+    /// @author SMM
+    /// @date 01/09/2017
+    LSDRaster return_as_raster();
+    
+    /// @brief This resizes the LSDRasterModel, resetting some flags in the process,
+    /// as well as setting many of the Array2D data members to be empty arrays
+    /// The raster data in the end is a random surface (determined by the noise
+    /// data member)
+    /// This overloaded version also changes the data resolution
+    /// @param new_rows the new number of rows
+    /// @param new_cols the new number of columns
+    /// @param new_resolution the new data resolution
+    /// @param new_value the new value of the raster
+    /// @author SMM
+    /// @date 01/09/2017
+    void resize_and_reset( int new_rows, int new_cols, float new_resolution, float new_value );
+
+    /// @brief Gets the minimum and maximum values from a raster
+    /// @return vector with the first element is the minimum and second is the maximum
+    /// @author SMM
+    /// @date 03/09/2017
+    vector<float> minimum_and_maximum_value();
+
+    /// @brief This linearly scales the raster to new minimum and maximum values
+    /// @param new_minimum does what it says on the tin. 
+    /// @param new_maxuimum does what it says on the tin
+    /// @author SMM
+    /// @date 03/09/2017
+    void scale_to_new_minimum_and_maximum_value(float new_minimum, float new_maximum);
+
+
+    /// @brief This smooths the raster. At some point in the future I'll
+    ///  add more options but at the moment it just uses 4 neighbours and has
+    ///  double weighting on the central pixel. It assumes periodic boundaries
+    ///  on the E/W
+    /// @param boundary type: this just goes to a default at the moment.
+    /// @author SMM
+    /// @date 03/09/2017
+    void smooth(int boundary_type);
+
+
+    //void random_horizontal_strips(int minimum_strip_size, int maximum_strip_size, float minimum_value, float maximum_value);
+    
+    /// @brief This makes square blobs randomly in the DEM
+    /// @param minimum_blob_size minimum blob size
+    /// @param maximum_blob_size maximum blob size
+    /// @param minimum_value minimum value on the raster
+    /// @param maximum_value maximum value on the raster
+    /// @parameter n_blobs the number of square blobs
+    /// @author SMM
+    /// @date 01/09/2017
+    void random_square_blobs(int minimum_blob_size, int maximum_blob_size, float minimum_value, float maximum_value, int n_blobs);
+
+    /// @brief This function adds some sine waves together to get patterns
+    /// @param x_coefficients Coefficients of the sin waves in the x direction.
+    /// @param y_coefficients Coefficients of the sin waves in the y direction.
+    /// @author SMM
+    /// @date 01/09/2017
+    void sine_waves(vector<float> x_coefficients, vector<float> y_coefficients);
+
+  protected:
+
+
+    
+  private:
+    /// @brief Make a 100x100 raster
+    void create();
+    
+    /// @brief Load a raster
+    void create(string filename, string extension);
+    
+    /// @brief Make a rastermaker from another raster
+    void create(LSDRaster& An_LSDRaster);
+    
+    /// @brief create a raster from NRows and NCols information
+    void create(int NRows, int NCols);
+
+};
+
+#endif
---
+++ src/driver_functions/MuddPILEdriver.make
@@ -0,0 +1,34 @@
+# MuddPILEdriver.make 
+# makes the master MuddPILEdriver program. 
+# make with: make -f MuddPILEdriver.make
+
+CC = g++
+CFLAGS= -c -I../../boost_mtl_minimal -Wall -O3
+OFLAGS = -I../../boost_mtl_minimal -Wall -O3
+LDFLAGS= -Wall
+SOURCES = MuddPILEdriver.cpp \
+		../LSDRasterSpectral.cpp \
+		../LSDIndexRaster.cpp \
+		../LSDShapeTools.cpp \
+		../LSDRaster.cpp \
+		../LSDRasterModel.cpp \
+		../LSDStatsTools.cpp \
+		../LSDFlowInfo.cpp \
+		../LSDParticle.cpp \
+    ../LSDRasterMaker.cpp \
+		../LSDParticleColumn.cpp \
+    ../LSDParameterParser.cpp \
+		../LSDCRNParameters.cpp
+SCRIPTS = animate.py
+OBJ = $(SOURCES:.cpp=.o)
+#LIBS = -lfftw3 -g -O0 -D_GLIBCXX_DEBUG
+LIBS = -lfftw3 -Wwrite-strings
+EXEC = MuddPILEdriver.out
+
+all: $(SOURCES) $(SCRIPTS) $(EXEC)
+
+$(EXEC): $(OBJ)
+	$(CC) $(OFLAGS) $(OBJ) $(LIBS) -o $@
+
+%.o: %.cpp
+	$(CC) $(CFLAGS) $< -o $@
